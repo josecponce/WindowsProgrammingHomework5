6c9ea3f30179543598def1fa615dc187bbc06cdd
Jose C Ponce <pepote5391@gmail.com>
2016-09-24 17:30:10 -0400

    Added the textFile entity class.

---
+++ Homework5/Entities/TextFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5.Entities {
+    [Serializable]
+    public class TextFile {
+        public string Text { get; set; }
+        public Color TextColor { get; set; }
+        public Font Font { get; set; }
+    }
+}
